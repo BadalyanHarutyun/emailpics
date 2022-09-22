ca7e4e910aba99c07e4185b0f77a0336b7ad80a1
BadalyanHarutyun <65309895+BadalyanHarutyun@users.noreply.github.com>
2022-09-22 17:15:07 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/:id',(req,res) => {
+app.get('/ro/:id',(req,res) => {
     console.log(req.params)
     res.sendFile(path.join(__dirname,'20220831_180021.jpg'))
 })
