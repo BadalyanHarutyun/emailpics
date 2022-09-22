12a0a0101e01e2748d1925525c63bb0515f221e6
BadalyanHarutyun <65309895+BadalyanHarutyun@users.noreply.github.com>
2022-09-22 17:07:03 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/',(req,res) => {
-    console.log('hi')
+app.get('/:id',(req,res) => {
+    console.log(req.params)
     res.sendFile(path.join(__dirname,'20220831_180021.jpg'))
 })
 app.listen(process.env.PORT || 1221, () => console.log('app listen'))
